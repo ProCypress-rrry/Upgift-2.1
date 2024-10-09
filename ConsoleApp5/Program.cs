02df6278b511ed66b3cf5a9c4678106f3c1c710e
Victor Johansson <vijo07001@utb.vaxjo.se>
2024-10-09 12:15:59 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿    using System;
+    namespace first_try
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok");
+            string bok = Console.ReadLine ();
+            Console.WriteLine("jo men" + " " + bok + " " + "är en bra klasiker");
+        }
+    }
+}  
